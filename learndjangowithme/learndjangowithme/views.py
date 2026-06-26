160f34ae915967543d4266acb62c8b841f8ee7ff
BhagirathsinhRana378 <bhagirathbhai378@gmail.com>
2026-06-26 18:36:35 +0530

    feat: add URL routing and view for thor page, update existing views and templates

--- learndjangowithme/learndjangowithme/views.py
+++ learndjangowithme/learndjangowithme/views.py
@@ -6,9 +6,12 @@ def home(request):
     #return HttpResponse("hello, YOur at the page")
     return render(request, 'website/index.html')
 
-def about(request):
-    #return HttpResponse("hiii, YOur at about the page")
-    return render(request, 'website/index2.html')
+# def about(request):
+#     #return HttpResponse("hiii, YOur at about the page")
+#     return render(request, 'website/index2.html')
 
-def contact(request):
-    return HttpResponse("hello, YOur at contact the page")
+# def contact(request):
+#     return HttpResponse("hello, YOur at contact the page")
+
+def thor(request):
+    return render(request, 'thor.html')
