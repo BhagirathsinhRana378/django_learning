fc1811b2cad7d463987250fb920cb506835fa5e2
BhagirathsinhRana378 <bhagirathbhai378@gmail.com>
2026-06-26 17:27:38 +0530

    feat: initialize Django project with basic views, templates, and static file configuration

---
+++ learndjangowithme/learndjangowithme/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    #return HttpResponse("hello, YOur at the page")
+    return render(request, 'website/index.html')
+
+def about(request):
+    return HttpResponse("hiii, YOur at about the page")
+
+def contact(request):
+    return HttpResponse("hello, YOur at contact the page")
